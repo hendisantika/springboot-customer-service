2865965dc6fabde9a29faf000d5355dd506ef7c5
Hendi Santika <hendisantika@gmail.com>
2019-08-05 20:08:10 +0700

    #3 - Add Customer Domain

---
+++ src/main/java/com/hendisantika/springbootcustomerservice/domain/Customer.java
@@ -0,0 +1,69 @@
+package com.hendisantika.springbootcustomerservice.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-customer-service
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-08-05
+ * Time: 20:07
+ */
+@Table(name = "CUSTOMER")
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = -6759774343110776659L;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "CUSTOMERID", updatable = false)
+    private Integer customerId;
+
+    @Column(name = "NAME")
+    private String customerName;
+
+    @Column(name = "DATEOFBIRTH", nullable = true)
+    private LocalDate dateofBirth;
+
+    @Column(name = "PHONENUMBER")
+    private String phoneNumber;
+
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDate getDateofBirth() {
+        return dateofBirth;
+    }
+
+    public void setDateofBirth(LocalDate dateofBirth) {
+        this.dateofBirth = dateofBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
+
