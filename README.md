958c643839595325f19f2277831754c48c01bd41
Hendi Santika <hendisantika@gmail.com>
2019-08-05 20:04:43 +0700

    #1 - Initial project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-customer-service
---
+++ src/main/java/com/hendisantika/springbootcustomerservice/SpringbootCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCustomerServiceApplication.class, args);
+    }
+
+}
