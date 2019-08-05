d20c348e82eb6cc5e68e5a676db3cb55dffc6337
Hendi Santika <hendisantika@gmail.com>
2019-08-05 20:20:35 +0700

    #16 - Add save method in CustomerController

--- src/main/java/com/hendisantika/springbootcustomerservice/controller/CustomerController.java
+++ src/main/java/com/hendisantika/springbootcustomerservice/controller/CustomerController.java
@@ -1,7 +1,12 @@
 package com.hendisantika.springbootcustomerservice.controller;
 
+import com.hendisantika.springbootcustomerservice.domain.Customer;
 import com.hendisantika.springbootcustomerservice.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,4 +23,10 @@ public class CustomerController {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @PostMapping(value = "/customers")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Customer save(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
 }
